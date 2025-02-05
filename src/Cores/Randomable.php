20b0c4552ba66fb4873b7ceb875868926e33b837
Ali M. Yavari <ali.m.yavari70@gmail.com>
2025-02-05 12:14:59 +0330

    feat: add Randomable trait to get random element(s) from an array

---
+++ src/Cores/Randomable.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AliYavari\PersianFaker\Cores;
+
+/**
+ * Trait for retrieving random element(s) from an array.
+ *
+ * @template TKey of array-key
+ * @template TValue
+ */
+trait Randomable
+{
+    /**
+     * Retrieves one random element from the provided array.
+     *
+     * @param  array<TKey, TValue>  $data
+     * @return TValue
+     */
+    protected function getOneRandomElement(array $data)
+    {
+        return $data[array_rand($data)];
+    }
+
+    /**
+     * Retrieves random elements from the provided array.
+     *
+     * @param  array<TKey, TValue>  $data
+     * @param  int  $number  The number of elements to return
+     * @return list<TValue>
+     */
+    protected function getMultipleRandomElements(array $data, int $number): array
+    {
+        $Elements = [];
+
+        for ($i = 1; $i <= $number; $i++) {
+            $Elements[] = $data[array_rand($data)];
+        }
+
+        return $Elements;
+    }
+}

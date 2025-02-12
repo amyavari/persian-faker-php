f8ed268f053c3a9da98e8b008f85347062298a1d
Ali M. Yavari <ali.m.yavari70@gmail.com>
2025-02-12 08:52:17 +0330

    feat: add compnay name faker class

---
+++ src/Fakers/Company/CompanyNameFaker.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AliYavari\PersianFaker\Fakers\Company;
+
+use AliYavari\PersianFaker\Fakers\SimplePicker;
+
+/**
+ * Generates a random company name of Iranian companies.
+ *
+ * @extends \AliYavari\PersianFaker\Fakers\SimplePicker<int, string>
+ */
+class CompanyNameFaker extends SimplePicker
+{
+    /**
+     * This returns a fake company name
+     */
+    public function generate(): string
+    {
+        return parent::generate();
+    }
+}
